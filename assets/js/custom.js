25512155d6975053208aa44a6eff97358b4134fa
unknown <techshuvo@gmail.com>
2020-09-11 01:03:48 +0600

    men,flass-sells section and other issue

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -12,17 +12,43 @@ $(function() {
       
       });
 
-      var feature = new Swiper('.feature_slider ', {
+      var feature = new Swiper('.feature-slider ', {
         slidesPerView: 3,
         slidesPerGroup: 1,
         loop: true,
         loopFillGroupWithBlank: true,
         navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
+          nextEl: '.feature-slider-next',
+          prevEl: '.feature-slider-prev',
+        },
+      });
+      
+      var flash = new Swiper('.flash-slider ', {
+        slidesPerView: 2,
+        slidesPerColumn: 2,
+        loop: true,
+        // loopFillGroupWithBlank: true,
+        navigation: {
+          nextEl: '.flash-slider-next',
+          prevEl: '.flash-slider-prev',
         },
       });
 
+
+      var client = new Swiper('.client-slider',{
+        slidesPerView:5,
+        slidesPerGroup: 1,
+        spaceBetween: 30,
+        loop:true,
+        loopFillGroupWithBlank: true,
+        pagination: {
+          el: '.client-pagination',
+          clickable: true,
+        },
+
+
+      });
+      
       var review = new Swiper('.review_slider',{
         slidesPerView:1,
         loop:true,
@@ -33,7 +59,7 @@ $(function() {
         },
 
 
-      })
+      });
 
 
 });
